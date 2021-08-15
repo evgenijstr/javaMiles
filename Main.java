c3b5f86308cb440cb914bc8656b952855cae56b3
Evgenijstr <evgenijstr@gmail.com>
2021-08-15 23:14:29 +0300

    Main.java added

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 20040;
+        int milePrice = 20;
+        int miles = ticketPrice / 20;
+        System.out.println(miles);
+    }
+}
