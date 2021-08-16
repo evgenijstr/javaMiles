42fa7fd15c4f049665515e5bd92d2f028243334b
Evgenijstr <evgenijstr@gmail.com>
2021-08-16 22:03:46 +0300

    variable milePrice is used

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         int ticketPrice = 20040;
         int milePrice = 20;
-        int miles = ticketPrice / 20;
+        int miles = ticketPrice / milePrice;
         System.out.println(miles);
     }
 }
